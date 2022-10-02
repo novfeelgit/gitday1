b56e28a0592e358eb8af94a22b036af517642569
novfeelgit <novfeelstar28@gmail.com>
2022-10-02 12:37:52 +0530

    gitday01

---
+++ src/org/Gitday1/Day1.java
@@ -0,0 +1,26 @@
+package org.Gitday1;
+
+
+public class Day1 {
+	private void add() {
+		// datatype varable name = value
+				
+				String name = "Novfeel";
+				byte age = 127;
+				long phone = 9688999284l;
+				char gender= 'm';
+				
+				System.out.println(name);
+				System.out.println(age);
+				System.out.println(phone);
+				System.out.println(gender);
+				
+				
+				System.out.println("My name is : "+name);
+			
+			}
+			public static void main(String[] args) {
+				Day1 a= new Day1();
+				a.add();
+			}
+}
